fa43797f87aa80600aaefec826049f9fa4e6a2c6
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2020-08-24 23:04:11 -0300

    setting message sender bot

--- src/bot.ts
+++ src/bot.ts
@@ -2,11 +2,11 @@ import puppeteer from "puppeteer";
 
 (async () => {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: false,
     });
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({path: 'example.png'});
-  
+    await page.goto('https://www.google.com');
+    await page.screenshot({ path: 'example.png' });
+
     await browser.close();
-  })();
+})();
